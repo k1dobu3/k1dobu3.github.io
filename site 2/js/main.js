62fdd7fdf552fdfb56f52234af6723c7a739813d
Kirill <91422323+k1dobu3@users.noreply.github.com>
2022-08-20 14:43:28 +0300

    Add files via upload

---
+++ site 2/js/main.js
@@ -0,0 +1,19 @@
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.micro__nav-btn')) {
+      var dropdowns = document.getElementsByClassName("burger-navbar");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
